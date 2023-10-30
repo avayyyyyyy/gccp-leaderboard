b6f3e4813d9bd8f18365ad855d3b2f5a338b7ca6
avayyyyyyy <jabhi465@gmail.com>
2023-10-30 19:29:09 +0530

    Day 27

--- src/components/Table.Index.js
+++ src/components/Table.Index.js
@@ -39,7 +39,7 @@ function TableIndex() {
     <div className="w-full relative px-3">
       <div className="sec m-auto my-10 space-y-8 w-1/2 mob:w-full flex flex-col">
         <div className="message bg-yellow-100 text-yellow-700 p-5 rounded-lg shadow-lg shadow-yellow-300/30 text-center border border-yellow-300/30">
-          <p className="text-center">-: Todays Quote :-</p>
+          {/* <p className="text-center">-: Todays Quote :-</p> */}
           <p>
             Gratitude to every student who contributed to the success of our
             inaugural Google event. Your participation was invaluable, and we
